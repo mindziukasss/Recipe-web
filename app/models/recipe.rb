b8f51c4b29e1e86780f16b5c68aa42e9d214358b
mindziukasss <mindziukasss@gmail.com>
2016-07-29 12:26:25 +0300

    Added Recipes with ingredients and directions in nestd forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,13 +3,13 @@ class Recipe < ActiveRecord::Base
 	has_many :directions
 
 	accepts_nested_attributes_for :ingredients,
-																reject_if: proc { |attribute| attributes['name'].blank? },
+																reject_if: proc { |attribute| attribute['name'].blank? },
 																allow_destroy: true
 	accepts_nested_attributes_for :directions,
-																reject_if: proc { |attribute| attributes['step'].blank? },
+																reject_if: proc { |attribute| attribute['step'].blank? },
 																allow_destroy: true
 
 	validates :title, :description, :image, presence: true
-  has_attached_file :image, styles: { :medium => "400x400#"}
+  	has_attached_file :image, styles: { :medium => "400x400#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
